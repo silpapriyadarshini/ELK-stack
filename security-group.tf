a3ae31c634e5481ce5dd4b7e57655cb0fdf639cb
silpapriyadarshini <silpa.priyadarshini@cloudreach.com>
2022-08-24 22:49:50 +0530

    modified code

--- security-group.tf
+++ security-group.tf
@@ -162,6 +162,14 @@ resource "aws_security_group" "demo_sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description      = "Allow port 5066"
+    from_port        = 5066
+    to_port          = 5066
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
   
   egress {
     from_port        = 0
